53d71540ab68618bc6f5ec7e9e34451828c4a8ee
Бакыт Шайырбеков <c0mrade@Bakyts-MacBook-Air.local>
2022-02-03 10:57:09 +0600

    feature/add_gitignore

--- product/models.py
+++ product/models.py
@@ -5,7 +5,6 @@ class Product(models.Model):
     title = models.CharField(max_length = 25)
     description = models.TextField()
     price = models.IntegerField()
-    image = models.ImageField()
     image = models.ImageField(
         upload_to='image/%Y/%m/%d',
         blank=True
